a2a04fab7f0034bd72211ee7316ec3555f7f2c02
AkimFeopentov2003 <akimses@gmail.com>
2022-09-27 21:29:14 +0300

    Hello, Alex

---
+++ Lesson3.py
@@ -0,0 +1,60 @@
+import sys
+
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        left = self.left.key if self.left else None
+        right = self.right.key if self.right else None
+        return 'key: {}, left: {}, right: {}'.format(self.key, left, right)
+
+
+def read_data():
+    n = int(input())
+    nodes = [Node(0) for _ in range(n)]
+    for i in range(n):
+        key, left, right = map(int, sys.stdin.readline().split())
+        nodes[i].key = key
+        if left != -1:
+            nodes[i].left = nodes[left]
+        if right != -1:
+            nodes[i].right = nodes[right]
+    return nodes[0]
+
+
+def in_oder(current, result):
+    if not current:
+        return
+    result.append(current.key)
+    in_oder(current.left, result)
+    in_oder(current.right, result)
+    return result
+
+
+def breadth_first_search(root):
+    queue = [root]
+    while queue:
+        tmp_queue = []
+        for element in queue:
+            print(element.key, end=' ')
+            if element.left:
+                tmp_queue.append(element.left)
+            if element.right:
+                tmp_queue.append(element.right)
+        queue = tmp_queue
+        print()
+
+
+def main():
+    root = read_data()
+    print(in_oder(root, []))
+    print()
+    breadth_first_search(root)
+
+
+
+main()
